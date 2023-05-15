fef68c8a957fe0d3f836c0861a6d5410e0db4944
jpinillag <47644592+jpinillag@users.noreply.github.com>
2023-05-14 22:25:09 -0500

    intento 3

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,13 +1,18 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
-from functools import reduce
+import sys
 
-def conteo(inicial, siguiente):
-    inicial[siguiente] = inicial.get(siguiente, 0) + 1
-    return inicial
- 
-data=sys.stdin
+diccionario_conteo = {}
 
-sys.stdout.write(reduce(conteo,data,{})) 
+for credit_history in sys.stdin:
+  if credit_history in diccionario_conteo.keys():
+    diccionario_conteo[credit_history] += 1
+  else:
+    diccionario_conteo[credit_history] = 1
+    
+for clave in diccionario_conteo.keys():
+   linea =  clave + "\t" + str(dict[clave]) + "\n"
+   sys.stdout.write(linea)  
+
 
