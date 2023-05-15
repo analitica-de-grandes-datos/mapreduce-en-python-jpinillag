20c0a2d56727c22e25baf53b196a042f2d7b20af
jpinillag <47644592+jpinillag@users.noreply.github.com>
2023-05-14 21:43:38 -0500

    Intento 1
    Primera solucion

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import csv
+
+def separar_depurar(x):
+    return(x.split(sep=',')[2])
+
+
+def map_pregunta1():
+    
+    ruta_archivo="C:/Users/Open/Desktop/Universidad/mapreduce-en-python-jpinillag/pregunta_01/credit.csv"
+    archivo_leido=[]
+    with open(ruta_archivo,"rt") as archivo:
+        for line in archivo:
+            archivo_leido.append(line)
+    return list(map(separar_depurar,archivo_leido))
+
+map_pregunta1()
