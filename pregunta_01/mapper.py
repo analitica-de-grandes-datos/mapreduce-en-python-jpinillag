59d7ab6e04373a899b642b2a81901297985655f9
jpinillag <47644592+jpinillag@users.noreply.github.com>
2023-05-14 22:04:53 -0500

    Intento 3
    Cambio la ruta en especifico, para que me reciba lo que sea

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -2,18 +2,17 @@
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 import csv
+import sys
 
 def separar_depurar(x):
     return(x.split(sep=',')[2])
 
+ruta_archivo=sys.stdin
+archivo_leido=[]
+with open(ruta_archivo,"rt") as archivo:
+    for line in archivo:
+        archivo_leido.append(line)
+        
+sys.stdout.write(list(map(separar_depurar,archivo_leido)))
 
-def map_pregunta1():
-    
-    ruta_archivo="credit.csv"
-    archivo_leido=[]
-    with open(ruta_archivo,"rt") as archivo:
-        for line in archivo:
-            archivo_leido.append(line)
-    return list(map(separar_depurar,archivo_leido))
 
-map_pregunta1()
