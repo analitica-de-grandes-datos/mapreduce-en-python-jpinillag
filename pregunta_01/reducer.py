e47b0fa36a5fa244eba8ddb0d683f30f172c6339
jpinillag <47644592+jpinillag@users.noreply.github.com>
2023-05-14 21:45:24 -0500

    Intento 1
    Primera posible respuesta

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+from functools import reduce
+
+def conteo(inicial, siguiente):
+    inicial[siguiente] = inicial.get(siguiente, 0) + 1
+    return inicial
+ 
+def reducer_pregunta1(data):
+  return reduce(conteo,data,{})
