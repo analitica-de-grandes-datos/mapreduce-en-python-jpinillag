a5eb489d6f07637b82fa804dbf1a403c46e331bb
jpinillag <47644592+jpinillag@users.noreply.github.com>
2023-05-16 21:50:31 -0500

    Ultimo update
    se corrige el formato de salida

--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -5,16 +5,15 @@ import sys
 
 diccionario_conteo = {}
 
-for credit_history in sys.stdin:
-  if credit_history in diccionario_conteo.keys():
-    diccionario_conteo[credit_history] += 1
+for historia_credito in sys.stdin:
+  if historia_credito in diccionario_conteo.keys():
+    diccionario_conteo[historia_credito] += 1
   else:
-    diccionario_conteo[credit_history] = 1
+    diccionario_conteo[historia_credito] = 1
     
 for clave in diccionario_conteo.keys():
   clave_edit=clave.strip()
   string_valor=str(diccionario_conteo[clave])
-  linea =  clave_edit  + "\t" + string_valor + "\n"
-  sys.stdout.write(linea)  
+  print(clave_edit  + "\t" + string_valor + "\n")  
 
 
