0c347b50226592e95d52ead856cfbd328167733a
jpinillag <47644592+jpinillag@users.noreply.github.com>
2023-05-16 21:49:58 -0500

    Actualizacion final mapper
    se corrige el formato de salida

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -5,8 +5,7 @@ import sys
 
 
 for fila in sys.stdin:
-  fila_separada = fila.split(",")
-  credit_history = fila_separada[2]
-  sys.stdout.write(credit_history+"\n")
+  fila_separada=fila.split(",")
+  print(fila_separada[2]+"\n")
 
 
