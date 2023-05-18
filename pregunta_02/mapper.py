62b74c17c6f914239e37303bdc5a41ceb8c6842c
jpinillag <47644592+jpinillag@users.noreply.github.com>
2023-05-17 19:34:56 -0500

    Update mapper inicial
    Se agraga la primera opción de respuesta

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+
+for fila in sys.stdin:
+  fila_separada=fila.split(",")
+  sys.stdout.write(fila_separada[3]+","+fila_separada[4]+ "\n")
