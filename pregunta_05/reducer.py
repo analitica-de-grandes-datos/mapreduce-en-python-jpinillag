0ae0008427804958bcbf3bf43d4eebb72cd54a3d
jpinillag <47644592+jpinillag@users.noreply.github.com>
2023-05-17 21:06:41 -0500

    Update reducer inicial pregunta 5
    Respuesta inicial

--- pregunta_05/reducer.py
+++ pregunta_05/reducer.py
@@ -1,3 +1,15 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+diccionario_conteo = {}
+
+for letra in sys.stdin:
+  if letra in diccionario_conteo.keys():
+    diccionario_conteo[letra] += 1
+  else:
+    diccionario_conteo[letra] = 1
+    
+for clave in diccionario_conteo.keys(): 
+  print(clave.replace("\n","") + "\t"+str(diccionario_conteo[clave])) 
