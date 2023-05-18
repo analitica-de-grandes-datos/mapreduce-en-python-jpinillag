5dbc61e124497194b81a13525e3f813e50b28402
jpinillag <47644592+jpinillag@users.noreply.github.com>
2023-05-18 18:40:08 -0500

    Update reducer pregunta 8
    Respuesta inicial

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -3,6 +3,19 @@
 #
 import sys
 
-for fila in sys.stdin:
-  fila_separada=fila.split("   ")
-  sys.stdout.write(fila_separada[0]+","+fila_separada[2])
+diccionario_conteo = {}
+diccionario_suma={}
+
+for linea in sys.stdin:
+  linea_separada=linea.split(",")
+  if linea_separada[0] in diccionario_conteo.keys():
+    diccionario_conteo[linea_separada[0]] += 1
+    diccionario_suma[linea_separada[0]]+=int(linea_separada[1])
+  else:
+    diccionario_conteo[linea_separada[0]] = 1
+    diccionario_suma[linea_separada[0]]=int(linea_separada[1])
+    
+for clave in diccionario_conteo.keys():
+  suma=str(diccionario_suma[clave])
+  promedio=str(diccionario_suma[clave]/diccionario_conteo[clave])
+  sys.stdout.write(clave+"	"+suma+"	"+promedio+"\n")
