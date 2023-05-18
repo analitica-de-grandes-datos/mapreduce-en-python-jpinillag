aee657a5eb3dc029c22d222d31801344f88ae276
jpinillag <47644592+jpinillag@users.noreply.github.com>
2023-05-17 19:36:34 -0500

    Update reducer inicial
    Respuesta inicial para el reducer

--- pregunta_02/reducer.py
+++ pregunta_02/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+diccionario_maximo = {}
+
+for linea in sys.stdin:
+    linea_separada=linea.split(",")
+    if   linea_separada[0] in diccionario_maximo.keys():
+        if diccionario_maximo[linea_separada[0]] < int(linea_separada[1]):
+            diccionario_maximo[linea_separada[0]] = int(linea_separada[1])
+    else:
+        diccionario_maximo[linea_separada[0]] = int(linea_separada[1])
+
+for clave in diccionario_maximo.keys():
+  clave_edit=clave.strip()
+  string_valor=str(diccionario_maximo[clave])
+  print(clave_edit  + "\t" + string_valor)
