408d0382e4265e35e479249862e335282aec741b
jpinillag <47644592+jpinillag@users.noreply.github.com>
2023-05-17 21:49:24 -0500

    Update reducer inicial pregunta 7
    Respuesta inicial

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+list=[]
+
+for fila in sys.stdin:
+    fila_separada=fila.split(",")
+    list.append((fila_separada[0],fila_separada[1],int(fila_separada[2])))
+    list.sort(key=lambda x: x[2])
+    list.sort(key=lambda x: x[0])
+
+
+for registro in list:
+  sys.stdout.write(registro[0] + "\t" + registro[1]+ "\t" + str(registro[2])+"\n") 
